4eef1e25d4635d4d02e3b8c5bfa07b353ede8e33
TBoot <thibateau@gmail.com>
2022-06-08 17:28:17 +0200

    new navbar responsive

--- components/TNavBar.vue
+++ components/TNavBar.vue
@@ -1,30 +1,18 @@
 <template>
-<div>
-    <nav class="bg-white shadow dark:bg-gray-800">
-        <div class="container px-6 py-4 mx-auto lg:flex lg:justify-between lg:items-center">
-            <div class="lg:flex lg:items-center">
-                <div class="flex items-center justify-between">
-                    <div>
-                        <a class="text-2xl font-bold text-red-500 transition-colors duration-200 transform dark:text-white lg:text-3xl hover:text-red--700 dark:hover:text-gray-300" href="#">SPICY-ROCKET</a>
-                    </div>
-
-                    <!-- Mobile menu button -->
-                    <div class="flex lg:hidden">
-                        <button type="button" class="text-gray-500 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400" aria-label="toggle menu">
-                            <svg viewBox="0 0 24 24" class="w-6 h-6 fill-current">
-                                <path fill-rule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"></path>
-                            </svg>
-                        </button>
-                    </div>
-                </div>
-
-                <div class="flex flex-col text-gray-600 capitalize dark:text-gray-300 lg:flex lg:px-16 lg:-mx-4 lg:flex-row lg:items-center">
-                    <a href="#" class="mt-2 transition-colors duration-200 transform lg:mt-0 lg:mx-4 hover:text-gray-900 dark:hover:text-gray-200">Produits</a>
-                    <a href="#" class="mt-2 transition-colors duration-200 transform lg:mt-0 lg:mx-4 hover:text-gray-900 dark:hover:text-gray-200">Goodies</a>
-                    <a href="#" class="mt-2 transition-colors duration-200 transform lg:mt-0 lg:mx-4 hover:text-gray-900 dark:hover:text-gray-200">Nos avis</a>
-                </div>
-            </div>
-
+  <header class="bg-white">
+    <nav class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8" aria-label="Top">
+      <div class="w-full py-6 flex items-center justify-between border-b border-white lg:border-none">
+        <div class="flex items-center">
+          <a href="#">
+            <span class="sr-only">Spicy-Rocket</span>
+            <img class="h-10 w-auto" src="/images/logochili.png" alt="" />
+          </a>
+          <div class="hidden ml-10 space-x-8 lg:block">
+            <a v-for="link in navigation" :key="link.name" :href="link.href" class="text-base font-medium text-black hover:text-green-500">
+              {{ link.name }}
+            </a>
+          </div>
+        </div>
             <div class="flex justify-center mt-6 lg:flex lg:mt-0 lg:-mx-2">
                 <a href="#" class="mx-2 text-gray-600 transition-colors duration-200 transform dark:text-gray-300 hover:text-gray-500 dark:hover:text-gray-300" aria-label="Reddit">
                 <img class="w-6 h-6" src="/images/fb.gif">
@@ -36,7 +24,20 @@
                 <img class="w-6 h-6" src="/images/twitter.gif">
                 </a>
             </div>
-        </div>
+      </div>
+      <div class="py-4 flex flex-wrap justify-center space-x-6 lg:hidden">
+        <a v-for="link in navigation" :key="link.name" :href="link.href" class="text-base font-medium text-black hover:text-green-500">
+          {{ link.name }}
+        </a>
+      </div>
     </nav>
-</div>
+  </header>
 </template>
+
+<script setup>
+const navigation = [
+  { name: 'Produits', href: '#' },
+  { name: 'Vos avis', href: '#' },
+  { name: 'Goodies', href: '#' },
+]
+</script>
